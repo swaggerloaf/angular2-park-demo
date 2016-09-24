bb3de7b35bfaf272213948a22699497e36271e27
swaggerloaf <glennrebel17@gmail.com>
2016-09-24 16:45:17 -0400

    Cleanup

--- app/main.ts
+++
@@ -1,9 +0,0 @@
-// initializes our platform
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { RootAppModule } from './park/app.module';
-
-const myPlatform = platformBrowserDynamic();
-
-// bootstrap the root module
-myPlatform.bootstrapModule(RootAppModule)
-    .then();
